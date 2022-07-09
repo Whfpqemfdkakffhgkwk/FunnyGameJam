1c246144073c1ea644f23a4ce2ef01f1745e8e1a
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2022-07-09 21:52:13 +0900

    타이틀 효과음 추가

--- 2022SchoolGameJam/Assets/JaeWon/Script/UI_SC/StartButton.cs
+++ 2022SchoolGameJam/Assets/JaeWon/Script/UI_SC/StartButton.cs
@@ -6,13 +6,22 @@ using DG.Tweening;
 
 public class StartButton : MonoBehaviour
 {
+    public SoundManager soundManager;
+    AudioSource audioSource;
     float fadeDuration = 2;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     public void OnClickExit()
     {
+        soundManager.PlaySound("JUMP");
         Application.Quit();
     }
     public void OnClickStart()
     {
+
+        soundManager.PlaySound("JUMP");
         SceneManager.LoadScene("BreakBlockTest");
     }
 }

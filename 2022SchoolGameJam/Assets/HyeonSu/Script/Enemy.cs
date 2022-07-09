84578fe79dd9e52c6c3e885b9206e481c315f3ec
qkrgustn <sdh210212@sdh.hs.kr>
2022-07-09 22:23:58 +0900

    라운드 완성

--- 2022SchoolGameJam/Assets/HyeonSu/Script/Enemy.cs
+++ 2022SchoolGameJam/Assets/HyeonSu/Script/Enemy.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
--- 2022SchoolGameJam/Assets/HyeonSu/Script/GameManager.cs
+++ 2022SchoolGameJam/Assets/HyeonSu/Script/GameManager.cs
@@ -1,21 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class GameManager : MonoBehaviour
 {
-    GameObject[] Enemys; 
+    [SerializeField] GameObject[] EnemyTypes;
+    GameObject[] Enemys;
+    int EnemyNum;
+    [SerializeField] Transform[] EnemyRecallPoss;
     public void NextTurn()
     {
         Recall();
         Enemys = GameObject.FindGameObjectsWithTag("Enemy");
         for (int i = 0; i < Enemys.Length; i++)
         {
-
+            Enemys[i].gameObject.transform.DOMove(Enemys[i].transform.position - new Vector3(0, 1.5f, 0), 1f);
         }
     }
     void Recall()
     {
-
+        EnemyNum = Random.Range(1, 4);
+        for (int i = 0; i < EnemyNum; i++)
+        {
+            int RandomRecallNum = Random.Range(0, 4);
+            if (EnemyRecallPoss[RandomRecallNum] != null)
+            {
+                Instantiate(EnemyTypes[Random.Range(0,2)], EnemyRecallPoss[RandomRecallNum].localPosition,
+                                    EnemyRecallPoss[RandomRecallNum].localRotation);
+            }
+            else
+            {
+                EnemyRecallPoss[RandomRecallNum] = null;
+                i -= 1;
+            }
+        }
+        for(int i = 0; i < 3; i++)
+        {
+            EnemyRecallPoss[i] = GameObject.Find($"Pos{i}").gameObject.transform;
+        }
     }
 }
